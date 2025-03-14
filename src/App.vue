cde632344d781609a5a5c8977c49b0d92355437e
Foursqweez <jeerasak.code@gmail.com>
2025-03-14 17:39:20 +0700

    Scoped & Global style

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name" />
-  <button @click="handleClick">Click me</button>
-  <Counter />
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
       title: 'My First Vue app!',
     }
-  },
-  methods: {
-    handleClick() {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    },
   },
 }
 </script>
@@ -29,12 +23,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #ddd;
+  color: #2c3e50;
   margin-top: 60px;
 }
-body {
-  background-color: #2c3e50;
-}
+
 h1 {
   border-bottom: 1px solid #ddd;
   padding-bottom: 10px;
